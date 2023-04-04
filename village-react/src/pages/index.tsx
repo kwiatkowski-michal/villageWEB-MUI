5406a1d38de25b60838476670cd75ad6d041d42e
kwiatkowski-michal <kwiatkowskimichal04@gmail.com>
2023-04-04 14:23:29 +0200

    a

---
+++ village-react/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './NavBar'
+import { Box, AppBar, Toolbar, Typography, Button } from '@mui/material'
+
+
+export default function Home() {
+  return (
+    <>
+      <NavBar />
+    </>
+  )
+}
