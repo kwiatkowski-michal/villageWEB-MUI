987c64a10104cef559452f24888183982b053439
Michał Kwiatkowski <kwiatkowskimichal04@gmail.com>
2023-04-05 19:39:29 +0200

    PWA support

--- village-react/pages/_app.tsx
+++ village-react/pages/_app.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Head from 'next/head';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -7,22 +6,30 @@ import { CacheProvider, EmotionCache } from '@emotion/react';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
 import NavBar from '../src/NavBar';
+import Head from 'next/head';
+
+
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
+
+
+
 
 export interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
 }
 
 export default function MyApp(props: MyAppProps) {
+
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
+    
     <CacheProvider value={emotionCache}>
       <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <link rel="manifest" href="/manifest.json" />
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
+
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <NavBar />
