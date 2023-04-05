65987bfa62bd84171be4faf936aa145ba3077c19
Michał Kwiatkowski <kwiatkowskimichal04@gmail.com>
2023-04-05 19:52:04 +0200

    working PWA

--- village-react/next.config.js
+++ village-react/next.config.js
@@ -1,15 +1,16 @@
+/** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
-    removeConsole: process.env.NODE_ENV !== "development",
-  },
-};
+    removeConsole: process.env.NODE_ENV !== 'development',
+  }
+}
 
-const withPWA = require("next-pwa")({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
+const withPWA = require('next-pwa')({
+  dest: 'public',
   register: true,
-});
+})
 
-module.exports = withPWA(nextConfig);
+module.exports = withPWA(nextConfig)
