a1aaec14f35d5601c9a455fcd8e946203dd83aa4
Michał Kwiatkowski <kwiatkowskimichal04@gmail.com>
2023-04-08 12:16:57 +0200

    minor changes in query

--- village-react/src/DocumentsDatabase.tsx
+++ village-react/src/DocumentsDatabase.tsx
@@ -1,49 +1,51 @@
 import React, { useState, useEffect } from "react";
 import { supabase } from "../utils/supabaseClient";
 import { Database } from "../types/supabase";
-import { Card, CardMedia, CardContent, Typography, CardActions, Button, Grid, Container, Box } from "@mui/material";
-
-
+import { Card, CardMedia, CardContent, Typography, CardActions, Button, Grid, Container, Box, Tooltip, IconButton } from "@mui/material";
+import DownloadRoundedIcon from '@mui/icons-material/DownloadRounded';
 export default function DokumentyDesktop() {
   const [documents, setDocuments] = useState<Database['public']['Tables']['documents']['Row'][]>([]);
-
+  
   useEffect(() => {
     async function fetchData() {
-      const { data, error } = await supabase.from('documents').select('*') as { data: Database['public']['Tables']['documents']['Row'][]; error: any };
+      const { data, error } = await supabase.from('documents').select('*').order('id', { ascending: false })
       if (error) {
         console.log(error);
       } else {
-        setDocuments(data);
+        setDocuments(data as Database['public']['Tables']['documents']['Row'][]);
+        console.log(data);
       }
     }
     fetchData();
   }, []);
 
   return (
-    <Box mt={4}>
+    <Box my={4}>
     <Container fixed>
-    <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+    <Grid container item spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
       {documents.map((document) => (
+        
         <Grid key={document.id} item xs={12} sm={4} md={4}>
-        <Card sx={{minHeight: "100%"}}>
+        <Card sx={{minHeight: "100%", position: 'relative'}}>
           <CardMedia
           component="img"
             image={document.card_media}
           />
           <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {document.title}
+          <Typography variant="overline" color="text.secondary">
+              Data publikacji: {document.created_at}
             </Typography>
             <Typography variant="body1" color="text.secondary">
               {document.description}
             </Typography>
-            
           </CardContent>
-          <CardActions >
           {document.dl_link && (
-              <Button href={document.dl_link} size="medium">POBIERZ PISMO</Button>
+            <CardActions sx={{marginTop:"1.8rem"}}>
+              <Tooltip title="Pobierz pismo" arrow leaveDelay={200} >
+              <IconButton href={document.dl_link} target="_blank" sx={{position: 'absolute', right: 10, bottom: 10}} size="medium"><DownloadRoundedIcon/></IconButton>
+              </Tooltip>
+              </CardActions>
             )}
-          </CardActions>
         </Card>
         </Grid>
       ))}
